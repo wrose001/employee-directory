15845fede85eb79fb170d85c0efc951994d7f847
Weston Rose <westonrose@Westons-MacBook-Air.local>
2020-04-29 20:21:09 -0500

    Removed build from gitignore.

---
+++ build/precache-manifest.73b2bb3b2c8cec652dd2caa166d01a22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97ebb9f8401462e02bc894773c28ec12",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "c637ca2a61973c6f46f3",
+    "url": "/employee-directory/static/css/main.48e0a752.chunk.css"
+  },
+  {
+    "revision": "1f243624e5ba8caa608e",
+    "url": "/employee-directory/static/js/2.6b6293aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory/static/js/2.6b6293aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c637ca2a61973c6f46f3",
+    "url": "/employee-directory/static/js/main.db762557.chunk.js"
+  },
+  {
+    "revision": "7a714ca12cb5c1ad059f",
+    "url": "/employee-directory/static/js/runtime-main.002bf0bf.js"
+  }
+]);
